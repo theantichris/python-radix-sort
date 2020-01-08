d2e544f4e5171ba924755d5c13131ca0d49a24bd
Christopher <chris@theantichris.com>
2020-01-08 07:31:39 -0500

    created buckets for each digit

--- radix.py
+++ radix.py
@@ -3,4 +3,12 @@ def radix_sort(to_be_sorted):
     maximum_value = max(to_be_sorted)
     max_exponent = len(str(maximum_value))
 
-    return max_exponent
+    # create a copy of the list
+    being_sorted = to_be_sorted[:]
+
+    # create buckets for each digit
+    digits = [[] for digit in range(10)]
+
+    return digits
+
+print(radix_sort([1]))
