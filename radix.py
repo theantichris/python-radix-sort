bb64380320b63b1e983fa93ae0142c80680951f2
Christopher <chris@theantichris.com>
2020-01-08 08:35:50 -0500

    looping through each digit based on largest number

--- radix.py
+++ radix.py
@@ -1,26 +1,33 @@
 def radix_sort(to_be_sorted):
-    # finding the length of the largest number
+    # finding the number of digits of the largest number
     maximum_value = max(to_be_sorted)
     max_exponent = len(str(maximum_value))
 
     # create a copy of the list
     being_sorted = to_be_sorted[:]
 
-    # create buckets for each digit
-    digits = [[] for i in range(10)]
+    # loop through each digit based on largest exponent
+    for exponent in range(max_exponent):
+        # create buckets for each digit
+        digits = [[] for i in range(10)]
 
-    # placing a number into a bucket based on LSD
-    for number in being_sorted:
-        number_as_a_string = str(number)
-        digit = number_as_a_string[-1]
-        digit = int(digit)
-        digits[digit].append(number)
+        # get index for the number as a string
+        position = exponent + 1
+        index = -position
 
-    # adding numbers from digits as they appear
-    being_sorted = []
-    for numeral in digits:
-        being_sorted.extend(numeral)
+        # placing a number into a bucket based on LSD
+        for number in being_sorted:
+            number_as_a_string = str(number)
+            digit = number_as_a_string[index]
+            digit = int(digit)
+            digits[digit].append(number)
+
+        # adding numbers from digits as they appear
+        being_sorted = []
+        for numeral in digits:
+            being_sorted.extend(numeral)
 
     return being_sorted
 
-print(radix_sort([100, 201, 4278]))
+my_list = [234, 3981, 1, 48276, 23]
+print(radix_sort(my_list))
