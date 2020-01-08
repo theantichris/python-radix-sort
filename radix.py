4b1229559fceb41f79912432a2267d734f8f9e20
Christopher <chris@theantichris.com>
2020-01-08 07:25:18 -0500

    finding the max exponent

---
+++ radix.py
@@ -0,0 +1,6 @@
+def radix_sort(to_be_sorted):
+    # finding the length of the largest number
+    maximum_value = max(to_be_sorted)
+    max_exponent = len(str(maximum_value))
+
+    return max_exponent
