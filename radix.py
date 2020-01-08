2ef4b638cec216ba5ec5f8e26e1167f22da5cdfa
Christopher <chris@theantichris.com>
2020-01-08 08:45:23 -0500

    clean up

--- radix.py
+++ radix.py
@@ -1,30 +1,21 @@
 def radix_sort(to_be_sorted):
-    # finding the number of digits of the largest number
-    maximum_value = max(to_be_sorted)
-    max_exponent = len(str(maximum_value))
+    max_exponent = len(str(max(to_be_sorted)))
 
-    # create a copy of the list
-    being_sorted = to_be_sorted[:]
+    being_sorted = to_be_sorted[:]  # create a copy of the list
 
     # loop through each digit based on largest exponent
     for exponent in range(max_exponent):
-        # create buckets for each digit
-        digits = [[] for i in range(10)]
+        digits = [[] for i in range(10)]  # create buckets for each digit
+        index = -(exponent + 1)  # get the string index for the number
 
-        # get index for the number as a string
-        position = exponent + 1
-        index = -position
-
-        # placing a number into a bucket based on LSD
-        for number in being_sorted:
+        for number in being_sorted:  # placing numbers into a bucket based on LSD
             number_as_a_string = str(number)
 
             try:
-                digit = number_as_a_string[index]
+                digit = int(number_as_a_string[index]) # getting digit at current place
             except IndexError:
                 digit = 0 # if number doesn't have this digit it is 0
 
-            digit = int(digit)
             digits[digit].append(number)
 
         # adding numbers from digits as they appear
